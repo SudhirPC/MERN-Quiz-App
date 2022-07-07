8b75f9bbd637a8240d3e74276cee40798d0e1741
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-07-07 19:17:47 +0530

    small quize app ready  with react
    after  collect more data , apply functionality  on that data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { QuizePage } from './Components/QuizePage.jsx';
+import { Home } from './Components/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-     <h1>Sudhir</h1>
+     <h1>Quize App</h1>
      <QuizePage/>
+     {/* <Home/> */}
     </div>
   );
 }

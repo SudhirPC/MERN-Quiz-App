c3a50d1c33d947003936aeac4005c141935e5080
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-07-10 12:02:12 +0530

    tailwind navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { QuizePage } from './Components/QuizePage.jsx';
 import { Home } from './Components/Home.jsx';
+import { Navbar } from './Components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
-     <h1>Quize App</h1>
+     {/* <h1 className="bg-red-700">Quize App</h1> */}
+     <Navbar/>
      <QuizePage/>
      {/* <Home/> */}
     </div>

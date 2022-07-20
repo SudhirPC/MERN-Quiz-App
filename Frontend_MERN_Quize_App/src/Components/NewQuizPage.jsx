b9408947b50b6f772c30adcb67ab1d560b58a2f2
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-07-20 23:06:11 +0530

    No of  right questions updated in count

--- Frontend_MERN_Quize_App/src/Components/NewQuizPage.jsx
+++ Frontend_MERN_Quize_App/src/Components/NewQuizPage.jsx
@@ -6,6 +6,14 @@ import { Questions } from "./Questions.jsx";
 export const NewQuizPage = () => {
   const data = useSelector((state) => state.mernQuize.QuizData);
   const [count, setCount] = useState(0);
+const handleAnswer = (index, e,el) => {
+    // console.log("e[index]",e[index])
+    if (el.answer[0][index] === el.correctAnswer) {
+      console.log("right answer")
+      setCount(count + 1);
+    }
+    console.log("option click");
+  };
 
   // ------taking path frommwindow object and compairing with the backend data
 
@@ -25,13 +33,13 @@ export const NewQuizPage = () => {
   // console.log("newfilter",filtertopicwise[0])
   const dispatch = useDispatch();
 
-  const handleAnswer = (i, index, e, index1) => {
-    if (e[index] === data[index1].correctAnswer) {
-      // console.log("right answer")
-      setCount(count + 1);
-    }
-    console.log("option click");
-  };
+  // const handleAnswer = (i, index, e, index1) => {
+  //   if (e[index] === data[index1].correctAnswer) {
+  //     // console.log("right answer")
+  //     setCount(count + 1);
+  //   }
+  //   console.log("option click");
+  // };
   const fetchQuizData = () => {
     dispatch(fetchQuizDataFrombackend());
   };
@@ -44,6 +52,39 @@ export const NewQuizPage = () => {
 
   return (
     <div className="">
+      <h1>count:{count}</h1>
+        <div>
+          {newfilterquestions?.map((el,index)=>{
+            return(
+            <div>
+              {/* <Questions prop={el}/> */}
+              <div className="flex border border-grey-200 pl-1 pt-2 pb-2 mt-2  mr-4">
+        <div className="ml-">
+                <p className="text-2xl font-normal">Question{el.que}</p>
+                {el?.answer[0]?.map((e,index)=>{
+                    // console.log("index",index)
+                  return(
+                    <div className="flex">
+                      <p className="mr-2">{index+1})</p>  
+                      <div className="cursor-pointer hoverOption" onClick={() => handleAnswer(index,e,el)}>
+                      <li className="text-xl li-option-tag">{e}</li>
+                      </div>
+                  </div>
+                  )
+                })}
+        </div>
+        </div>
+            </div>
+            )
+          })}
+        </div>
+    </div>
+  );
+};
+
+
+
+
       {/* <h4>count:{count}</h4> */}
       {/* {filtertopicwise.map((e,index)=>{
             // console.log("e",e)
@@ -79,16 +120,3 @@ export const NewQuizPage = () => {
             </div>
           )
         })} */}
-
-        <div>
-          {newfilterquestions?.map((el,index)=>{
-            return(
-            <div>
-              <Questions prop={el}/>
-            </div>
-            )
-          })}
-        </div>
-    </div>
-  );
-};

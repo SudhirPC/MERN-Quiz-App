5eb98d68cd086f2b710199e23efacc2ba827984c
Sudhir Chavhan <sudhirchavhan100@gmail.com>
2022-08-19 18:23:19 +0530

    Complete the Show all answer page

--- Frontend_MERN_Quize_App/src/Components/ShowAllAnswers.jsx
+++ Frontend_MERN_Quize_App/src/Components/ShowAllAnswers.jsx
@@ -1,42 +1,63 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const ShowAllAnswers = () => {
   const resultUser = useSelector((state) => state.mernQuize.result);
   const singleQuiz = useSelector((state) => state?.mernQuize.QuizData);
   const questionArr = singleQuiz[0]?.questionArray;
-  console.log("resultUser",resultUser)
+  console.log("resultUser", resultUser);
   return (
-    <div className="flex w-11/12 ml-24">
-      <div className=" w-3/6">
-        <h1>QUESTIONS</h1>
-        {questionArr?.map((e,index)=>{
-          return(
-            <div className="h-16 mt-4 border-2 ">
-              <p>{index+1}) {e.questions}</p>
-            </div>
-          )
-        })}
+    <div>
+      <div className="flex w-11/12 ml-16 mt-12  mb-1">
+        <div className=" w-6/12 ">
+          <div className="text-center">
+            <h1 className="text-xl font-medium font-serif text-red-600">
+              QUESTIONS
+            </h1>
+          </div>
+
+          {questionArr?.map((e, index) => {
+            return (
+              <div className="h-16 mt-4 border-2 pl-4 ">
+                <p>
+                  {index + 1}) {e.questions}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className=" w-3/12">
+        <div className="text-center">
+            <h1 className="text-xl font-medium font-serif text-red-600">
+            USER ANSWER
+            </h1>
+          </div>
+          {resultUser?.map((e) => {
+            return (
+              <div className="h-16 mt-4 border-2 text-center red">
+                <p>{e}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-3/12">
+        <div className="text-center">
+            <h1 className="text-xl font-medium font-serif text-red-600">
+            CORRECT ANSWER
+            </h1>
+          </div>
+          {questionArr?.map((e) => {
+            return (
+              <div className="h-16 mt-4 text-center border-2 red">
+                <p>{e.correctAnswer}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className=" w-2/6">
-        <h1>USER ANSWER</h1>
-        {resultUser?.map((e)=>{
-          return(
-            <div className="h-16 mt-4 border-2 red">
-              <p>{e}</p>
-            </div>
-          )
-        })}
-      </div>
-      <div className=" w-1/6">
-        <h1>CORRECT ANSWER</h1>
-        {questionArr?.map((e)=>{
-          return(
-            <div className="h-16 mt-4 border-2 red">
-              <p>{e.correctAnswer}</p>
-            </div>
-          )
-        })}
+      <div className="border-teal-500 w-36  border-2 p-1 pl-2  pr-2  finalresult ">
+      <Link to="/result"><button className="text-xl  font-bold">Final Marks</button></Link>   
       </div>
     </div>
   );
